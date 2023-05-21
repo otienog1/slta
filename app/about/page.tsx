a4a214d9df2e55850b53be8af8ecbac9851a9304
oluoko <otienog1@gmail.com>
2023-05-21 18:25:45 +0300

    mode destination

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,8 +12,8 @@ const AboutPage = async () => {
             <div className="text-center py-12 px-6 mt-12">
                 <div className="container mx-auto pl-6">
                     <h1 className="font-display font-bold text-5xl mb-20 uppercase">{page.acf.title}</h1>
-                    <div className="flex justify-center w-full gap-2 mb-20">
-                        {Array.from(page.acf.images).map(
+                    <div className="flex justify-center w-full gap-4 mb-20">
+                        {page.acf.images.map(
                             (image: any, i: any) => (
                                 <div className={`w-1/3 h-[calc(50vh)] relative overflow-hidden`} key={i}>
                                     <Image
@@ -33,7 +33,7 @@ const AboutPage = async () => {
                 <div className="container mx-auto">
                     <h1 className="font-display text-base font-extrabold mb-6 uppercase">{page.acf.our_story.title}</h1>
                     <div className="w-full md:w-1/2 mx-auto" dangerouslySetInnerHTML={{ __html: page.acf.our_story.content }}></div>
-                    <div className="w-1/2 mx-auto relative h-[90vh] mt-20">
+                    <div className="w-1/2 mx-auto relative h-[60vh] mt-20">
                         <Image
                             src={page.acf.our_story.url}
                             alt="starlyn thrilling adventures"
--- app/destinations/page.tsx
+++ app/destinations/page.tsx
@@ -1,6 +1,31 @@
+'use client'
+import { getPage } from "@/components/page"
+import Image from "next/image"
+
 const Destinations = async () => {
+    const page = await getPage(12)
+
     return (
-        <div className="container mx-auto">Destinations</div>
+        <>
+            <div className="container mx-auto flex gap-4 flex-wrap justify-center pt-12 px-6 mt-12">
+                <h1 className="font-display font-bold text-4xl mb-20 uppercase tracking-wider">{page.acf.title}</h1>
+            </div>
+            <div className="container mx-auto flex gap-4 flex-wrap justify-center pb-12 px-6 mb-12">
+                {Array.from(page.acf.destinations).map(({ destination }: any, i: any) => (
+                    <div className="w-1/4 h-96 relative" key={i}>
+                        <Image
+                            src={destination.image}
+                            alt="starlyn thrilling adventures"
+                            fill
+                            style={{ objectFit: "cover" }}
+                        />
+                        <p className="absolute top-1/2 text-center text-white text-3xl font-bold w-full uppercase -translate-y-1/2">
+                            {destination.name}
+                        </p>
+                    </div>
+                ))}
+            </div>
+        </>
     )
 }
 
