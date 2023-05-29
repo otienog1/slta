baeaf09ba749d6600d44d9ad5582472de32e9b77
Oluoko <otienog1@gmail.com>
2023-05-29 22:13:11 +0300

    media queries

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,9 +20,9 @@ export default function RootLayout({
       <body suppressHydrationWarning={true}>
         <>
           <Navbar />
-          <div>
+          <>
             {children}
-          </div>
+          </>
           <Footer />
         </>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ const HomePage = async () => {
 
     return (
         <main className="min-h-screen justify-center">
-            <div className="h-[calc(100vh-80px)] lg:h-[calc(100vh-240px)]">
+            <div className="md:h-[calc(100vh-80px)] 2xl:h-[calc(100vh-240px)]">
                 <Slider
                     slides={page.acf.section_1.slider}
                     autoplay={!0}
@@ -38,7 +38,7 @@ const HomePage = async () => {
             <div className="" style={{ backgroundColor: 'rgba(27, 118, 50, .1)' }}>
                 <div className="container mx-auto">
                     <div className="grid grid-cols-1 md:grid-cols-2">
-                        <div className="p-12 md:py-24 h-[calc(100vh-300px)]">
+                        <div className="p-12 md:py-24 h-screen 2xl:h-[calc(100vh-300px)]">
                             <div className="relative h-full w-full">
                                 <Image
                                     src={page.acf.section_3.image}
@@ -73,7 +73,7 @@ const HomePage = async () => {
                         </h2>
                         <p className="text-xl tracking-tight" data-config-id="auto-txt-2-3">Use and re-use tons of responsive sections to create the perfect layout. Sections are organised into convenient categories.</p>
                     </div>
-                    <div className="flex flex-wrap -m-9">
+                    <div className="flex flex-wrap px-4">
                         <div className="w-full md:w-1/2 lg:w-1/4 p-9">
                             <div className="group cursor-pointer">
                                 <div className="mb-6 overflow-hidden rounded-lg">
@@ -82,8 +82,8 @@ const HomePage = async () => {
                                 <p className="mb-3 text-indigo-500 font-semibold tracking-tight" data-config-id="auto-txt-3-3">Finance</p>
                                 <h3 className="mb-4 text-xl group-hover:text-gray-900 font-semibold group-hover:underline" data-config-id="auto-txt-4-3">10 tips for managing your money in the new year</h3>
                                 <span className="text-sm text-gray-600 tracking-tight" data-config-id="auto-txt-5-3">August 28, 2022</span>
-                            </div>
-                        </div>
+                            </div >
+                        </div >
                         <div className="w-full md:w-1/2 lg:w-1/4 p-9">
                             <div className="group cursor-pointer">
                                 <div className="mb-6 overflow-hidden rounded-lg">
@@ -117,12 +117,12 @@ const HomePage = async () => {
                     </div>
                 </div>
             </div>
-            <div className="container px-4 mx-auto text-center py-24 md:py-32">
+            <div className="container px-4 mx-auto text-center py-24 md:py-32" >
                 <h2 className="font-heading mb-5 text-6xl tracking-tighter">
                     {page.acf.section_4.title}
                 </h2>
                 <div
-                    className="text-xl tracking-tight"
+                    className="px-8 text-xl tracking-tight"
                     dangerouslySetInnerHTML={
                         {
                             __html: page.acf.section_4.additional_services
@@ -149,22 +149,20 @@ const HomePage = async () => {
                 </div>
                 <div className="bg-white p-12 md:p-24 flex justify-end items-center">123</div>
             </div> */}
-            <div>
-                <div className="py-24 lg:py-28 overflow-hidden" style={{ background: `url(${page.acf.section_7.image})`, backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
-                    <div className="container px-4 mx-auto">
-                        <div className="max-w-xl mx-auto text-center">
-                            <h2 className="font-heading mb-6 text-6xl text-white tracking-tighter capitalize" data-config-id="auto-txt-1-6">Book your adventure today!</h2>
-                            <p className="mb-8 text-xl text-gray-200 tracking-tight" data-config-id="auto-txt-2-6">Customize your experience with our tailored itineraries and enjoy the expertise of our professional guides.</p>
-                            <div className="inline-block px-5 py-4 text-gray-700 font-semibold tracking-tight bg-white hover:bg-gray-100 rounded-lg focus:ring-4 focus:ring-gray-200 transition duration-200" href="#" data-config-id="auto-txt-3-6">
-                                <Link href={'/about'}>
-                                    {page.acf.section_7.title}
-                                </Link>
-                            </div>
+            <div className="container mx-auto py-24 lg:py-28 overflow-hidden" style={{ background: `url(${page.acf.section_7.image})`, backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
+                <div className="container px-4 mx-auto">
+                    <div className="max-w-xl mx-auto text-center">
+                        <h2 className="font-heading mb-6 text-6xl text-white tracking-tighter capitalize" data-config-id="auto-txt-1-6">Book your adventure today!</h2>
+                        <p className="mb-8 text-xl text-gray-200 tracking-tight" data-config-id="auto-txt-2-6">Customize your experience with our tailored itineraries and enjoy the expertise of our professional guides.</p>
+                        <div className="inline-block px-5 py-4 text-gray-700 font-semibold tracking-tight bg-white hover:bg-gray-100 rounded-lg focus:ring-4 focus:ring-gray-200 transition duration-200" href="#" data-config-id="auto-txt-3-6">
+                            <Link href={'/contact'}>
+                                {page.acf.section_7.title}
+                            </Link>
                         </div>
                     </div>
                 </div>
             </div>
-        </main >
+        </main>
     )
 }
 
