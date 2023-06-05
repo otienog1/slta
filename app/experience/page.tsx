c75537ec217cfa681d679bc988bf6c9227a5c01a
Oluoko <otienog1@gmail.com>
2023-06-05 15:33:33 +0300

    experiences page

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,9 +1,36 @@
-const Experience = () => {
+import Link from "next/link"
+import Image from "next/image"
+import { getExperiences } from "@/components/page"
+
+const Experiences = async () => {
+    const experiences = await getExperiences()
     return (
-        <>
-            Experience
-        </>
+        <div className="container mx-auto py-10">
+            <h2 className="font-bold tracking-tighter text-2xl px-8 mb-10">Experiences</h2>
+            <div className="flex flex-wrap px-4">
+                {experiences.map((experience: any, i: any) => (
+                    <div className="w-full md:w-1/2 lg:w-1/4 p-4" key={i}>
+                        <div className="group cursor-pointer">
+                            <Link href={`/experience/${experience.acf.slug}`} >
+                                <div className="mb-4 rounded overflow-hidden h-52 relative">
+                                    <Image
+                                        className="w-full rounded transform hover:scale-105 transition duration-500"
+                                        src={experience.acf.image}
+                                        alt={experience.acf.title}
+                                        fill
+                                        style={{ objectFit: 'cover' }}
+                                    />
+                                </div>
+                            </Link>
+                            <p className="text-indigo-500 font-semibold tracking-tight">{experience.acf.destination}</p>
+                            <h3 className="text-xl group-hover:text-gray-900 font-semibold group-hover:underline">{experience.acf.title}</h3>
+                            <span className="text-sm text-gray-600 tracking-tight">{experience.acf.days}</span>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
-export default Experience
+export default Experiences
--- components/page.tsx
+++ components/page.tsx
@@ -18,4 +18,14 @@ async function getExperience(id: any) {
     return res.json()
 }
 
-export { getPage, getExperience }
+async function getExperiences() {
+    let res = await fetch(`https://api.starlynthrillingadventures.com/wp-json/wp/v2/experience?_fields=acf&acf_format=standard`)
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data');
+    }
+
+    return res.json()
+}
+
+export { getPage, getExperience, getExperiences }
